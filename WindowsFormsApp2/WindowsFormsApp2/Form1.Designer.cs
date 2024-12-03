97ea94d183f156210cc2995470f8a9b8a0f20838
Мисак Гркикян <110992990+mgrkikyan@users.noreply.github.com>
2024-12-03 20:01:52 +0500

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿partial class Form1
+{
+    private System.ComponentModel.IContainer components = null;
+    private System.Windows.Forms.Button StartButton;
+    private System.Windows.Forms.TextBox txtWriter1Input;
+    private System.Windows.Forms.TextBox txtWriter2Input;
+    private System.Windows.Forms.TextBox txtWriter1Log;
+    private System.Windows.Forms.TextBox txtWriter2Log;
+    private System.Windows.Forms.TextBox txtReader1Log;
+    private System.Windows.Forms.TextBox txtReader2Log;
+
+    private void InitializeComponent()
+    {
+        this.StartButton = new System.Windows.Forms.Button();
+        this.txtWriter1Input = new System.Windows.Forms.TextBox();
+        this.txtWriter2Input = new System.Windows.Forms.TextBox();
+        this.txtWriter1Log = new System.Windows.Forms.TextBox();
+        this.txtWriter2Log = new System.Windows.Forms.TextBox();
+        this.txtReader1Log = new System.Windows.Forms.TextBox();
+        this.txtReader2Log = new System.Windows.Forms.TextBox();
+        this.SuspendLayout();
+
+        // StartButton
+        this.StartButton.Location = new System.Drawing.Point(12, 12);
+        this.StartButton.Name = "StartButton";
+        this.StartButton.Size = new System.Drawing.Size(75, 23);
+        this.StartButton.TabIndex = 0;
+        this.StartButton.Text = "Start";
+        this.StartButton.UseVisualStyleBackColor = true;
+        this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+
+        // txtWriter1Input
+        this.txtWriter1Input.Location = new System.Drawing.Point(12, 50);
+        this.txtWriter1Input.Name = "txtWriter1Input";
+        this.txtWriter1Input.Size = new System.Drawing.Size(200, 20);
+        this.txtWriter1Input.TabIndex = 1;
+
+        // txtWriter2Input
+        this.txtWriter2Input.Location = new System.Drawing.Point(12, 90);
+        this.txtWriter2Input.Name = "txtWriter2Input";
+        this.txtWriter2Input.Size = new System.Drawing.Size(200, 20);
+        this.txtWriter2Input.TabIndex = 2;
+
+        // txtWriter1Log
+        this.txtWriter1Log.Location = new System.Drawing.Point(12, 130);
+        this.txtWriter1Log.Multiline = true;
+        this.txtWriter1Log.Name = "txtWriter1Log";
+        this.txtWriter1Log.ReadOnly = true;
+        this.txtWriter1Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        this.txtWriter1Log.Size = new System.Drawing.Size(200, 100);
+        this.txtWriter1Log.TabIndex = 3;
+
+        // txtWriter2Log
+        this.txtWriter2Log.Location = new System.Drawing.Point(12, 240);
+        this.txtWriter2Log.Multiline = true;
+        this.txtWriter2Log.Name = "txtWriter2Log";
+        this.txtWriter2Log.ReadOnly = true;
+        this.txtWriter2Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        this.txtWriter2Log.Size = new System.Drawing.Size(200, 100);
+        this.txtWriter2Log.TabIndex = 4;
+
+        // txtReader1Log
+        this.txtReader1Log.Location = new System.Drawing.Point(230, 130);
+        this.txtReader1Log.Multiline = true;
+        this.txtReader1Log.Name = "txtReader1Log";
+        this.txtReader1Log.ReadOnly = true;
+        this.txtReader1Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        this.txtReader1Log.Size = new System.Drawing.Size(200, 100);
+        this.txtReader1Log.TabIndex = 5;
+
+        // txtReader2Log
+        this.txtReader2Log.Location = new System.Drawing.Point(230, 240);
+        this.txtReader2Log.Multiline = true;
+        this.txtReader2Log.Name = "txtReader2Log";
+        this.txtReader2Log.ReadOnly = true;
+        this.txtReader2Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        this.txtReader2Log.Size = new System.Drawing.Size(200, 100);
+        this.txtReader2Log.TabIndex = 6;
+
+        // Form1
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(450, 360);
+        this.Controls.Add(this.txtReader2Log);
+        this.Controls.Add(this.txtReader1Log);
+        this.Controls.Add(this.txtWriter2Log);
+        this.Controls.Add(this.txtWriter1Log);
+        this.Controls.Add(this.txtWriter2Input);
+        this.Controls.Add(this.txtWriter1Input);
+        this.Controls.Add(this.StartButton);
+        this.Name = "Form1";
+        this.Text = "Thread Synchronization";
+        this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+public partial class Form1 : Form
+{
+    private Mutex mutex = new Mutex();
+    private Queue<string> sharedQueue = new Queue<string>();
+    private Thread writer1, writer2, reader1, reader2;
+    private bool isRunning = true;
+
+    public Form1()
+    {
+        InitializeComponent();
+        CheckForIllegalCrossThreadCalls = false;
+
+        // Добавьте обработчики событий
+        txtWriter1Input.KeyPress += txtWriter1Input_KeyPress;
+        txtWriter2Input.KeyPress += txtWriter2Input_KeyPress;
+    }
+
+    private void StartButton_Click(object sender, EventArgs e)
+    {
+        writer1 = new Thread(Writer1Process);
+        writer2 = new Thread(Writer2Process);
+        reader1 = new Thread(Reader1Process);
+        reader2 = new Thread(Reader2Process);
+
+        writer1.Start();
+        writer2.Start();
+        reader1.Start();
+        reader2.Start();
+    }
+
+    private void txtWriter1Input_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == (char)Keys.Enter)
+        {
+            string dataToWrite = txtWriter1Input.Text;
+            if (!string.IsNullOrEmpty(dataToWrite))
+            {
+                mutex.WaitOne();
+                sharedQueue.Enqueue($"Writer1: {dataToWrite}");
+                mutex.ReleaseMutex();
+
+                txtWriter1Log.AppendText($"Записано: {dataToWrite}\r\n");
+                txtWriter1Input.Clear();
+            }
+            e.Handled = true; // Предотвращаем звуковой сигнал при нажатии Enter
+        }
+    }
+
+    private void txtWriter2Input_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == (char)Keys.Enter)
+        {
+            string dataToWrite = txtWriter2Input.Text;
+            if (!string.IsNullOrEmpty(dataToWrite))
+            {
+                mutex.WaitOne();
+                sharedQueue.Enqueue($"Writer2: {dataToWrite}");
+                mutex.ReleaseMutex();
+
+                txtWriter2Log.AppendText($"Записано: {dataToWrite}\r\n");
+                txtWriter2Input.Clear();
+            }
+            e.Handled = true;
+        }
+    }
+
+    // Измените методы Writer1Process и Writer2Process
+    private void Writer1Process()
+    {
+        while (isRunning)
+        {
+            Thread.Sleep(100); // Просто держим поток активным
+        }
+    }
+
+    private void Writer2Process()
+    {
+        while (isRunning)
+        {
+            Thread.Sleep(100);
+        }
+    }
+
+    private void Reader1Process()
+    {
+        while (isRunning)
+        {
+            mutex.WaitOne();
+            if (sharedQueue.Count > 0)
+            {
+                string dataRead = sharedQueue.Dequeue();
+                txtReader1Log.AppendText($"Прочитано: {dataRead}\r\n");
+            }
+            mutex.ReleaseMutex();
+            Thread.Sleep(1000);
+        }
+    }
+
+    private void Reader2Process()
+    {
+        while (isRunning)
+        {
+            mutex.WaitOne();
+            if (sharedQueue.Count > 0)
+            {
+                string dataRead = sharedQueue.Dequeue();
+                txtReader2Log.AppendText($"Прочитано: {dataRead}\r\n");
+            }
+            mutex.ReleaseMutex();
+            Thread.Sleep(1000);
+        }
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        isRunning = false;
+    }
+
+
+}
